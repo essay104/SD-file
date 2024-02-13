8074653d2026d829910716471909c82814328f22
essay104 <aleixs104@naver.com>
2024-02-13 18:30:51 +0900

    240213

---
+++ 240213/script.js
@@ -0,0 +1,35 @@
+// const button = document.querySelector("button");
+// button.addEventListener("click", () => {
+//     alert("오늘도 화이팅입니다!");
+//     confirm("오늘도 화이팅 하시겠습니까?");
+//   prompt("당신의 이름을 입력해주세요");
+// });
+
+// const name = "김사도";
+// window.document.write("제 이름은" + name + "입니다");
+
+// var num;
+// num = 7;
+
+//let
+
+// const str = "text";
+// console.log(str);
+
+// const boolen = true;
+
+// const tilte = 최재천의 곤충사회
+// const title =  16200;
+// const dieivery = free
+// const author = 최재천
+// const pufilishingdatea=202410;
+
+// const result = prompt("당신의 나이를 입력하세요!")
+// console.log : (tenyear)
+
+// const tenyear = result + 10
+
+const result = cofirm("오늘도 점심 드실꺼죠");
+if(result = true) 
+}
+console.log(result);
